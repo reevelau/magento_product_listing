368e284103e04ca19823ddc758a1def252276407
Reeve Lau <reevelau@gmail.com>
2023-06-12 17:40:55 +0800

    code tidy up

--- lib/main.dart
+++ lib/main.dart
@@ -39,10 +39,11 @@ class _MyHomePageState extends State<MyHomePage> {
   final ProductApi productApi = ProductApi();
   final ScrollController _scrollController = ScrollController();
   final List<Product> _products = [];
+  int _currentPage = 1;
 
   void _onScroll() {
     if (_scrollController.position.pixels >= _scrollController.position.maxScrollExtent - 1000) {
-      productApi.start();
+      productApi.fetch(_currentPage++);
     }
   }
 
@@ -55,13 +56,13 @@ class _MyHomePageState extends State<MyHomePage> {
       });
     });
     _scrollController.addListener(_onScroll);
-    productApi.start();
+    productApi.fetch(_currentPage++);
   }
 
   @override
   void dispose() {
     _scrollController.dispose();
-    productApi.stop();
+    productApi.dispose();
     super.dispose();
   }
 
@@ -91,34 +92,33 @@ class _MyHomePageState extends State<MyHomePage> {
                 crossAxisCount: 2,
                 mainAxisSpacing: 0,
                 crossAxisSpacing: 0,
-                childAspectRatio: 0.5,
+                childAspectRatio: 0.6,
               ),
               delegate: SliverChildBuilderDelegate( (context,index) {
-              debugPrint('SliverChildBuilderDelegate ()');
-              if (index >= _products.length) {
-                return null;
-              }
-              var item = _products[index];
-              return Column(
-                
-                children: [
-                  Image.network(item.imageUrl.toString(), height: 184.0, width: 184.0, fit: BoxFit.fitHeight),
-                  SizedBox(width: 184, child: Text(item.name)),
-                  Expanded(child: Container()),
-                  OutlinedButton.icon(
-                    style: OutlinedButton.styleFrom(
-                      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
-                      foregroundColor: Colors.grey,
-                      side: const BorderSide(
-
-                        color: Colors.grey
-                        )  
-                    ),
-                    onPressed: (){}, icon: const Icon(Icons.shopping_cart), label: const Text('加入購物車'),
-                    )
-                ],
-              );
-            }, childCount: _products.length ),
+                debugPrint('SliverChildBuilderDelegate ()');
+                if (index >= _products.length) {
+                  return null;
+                }
+                var item = _products[index];
+                return Column(
+                  
+                  children: [
+                    Image.network(item.imageUrl.toString(), height: 184.0, width: 184.0, fit: BoxFit.fitHeight),
+                    SizedBox(width: 184, child: Text(item.name)),
+                    Expanded(child: Container()),
+                    OutlinedButton.icon(
+                      style: OutlinedButton.styleFrom(
+                        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+                        foregroundColor: Colors.grey,
+                        side: const BorderSide(
+                          color: Colors.grey
+                          )  
+                      ),
+                      onPressed: (){}, icon: const Icon(Icons.shopping_cart), label: const Text('加入購物車'),
+                      )
+                  ],
+                );
+              }, childCount: _products.length ),
             
             )
           ],
@@ -137,56 +137,54 @@ class Product {
   Product(this.id, this.name, this.imageUrl);
 }
 
-Future<List<Product>> fetchProducts({int page =0}) async {
-  try {
-    debugPrint('fetchProducts: page = $page');
-    var url = Uri(
-      scheme: 'https', 
-      host: magentoHost, 
-      path: '/rest/V1/products-render-info', queryParameters: { 
-        'searchCriteria[sortOrders][0][field]': 'entity_id',
-        'searchCriteria[sortOrders][0][direction]':'DESC',
-        'searchCriteria[pageSize]': '20', 
-        'searchCriteria[currentPage]': '$page', 
-        'storeId' :'0', 
-        'currencyCode': 'hkd' 
-      });
-
-    var resp = await http.get(url);
-    var map = jsonDecode( resp.body );
-
-    var ret = <Product>[];
-    for(var item in map['items']) {
-      //print('name: ${item['name']}');
-      var image = item['images'].first;
-      //print(' url: ${image['url']}');
-      ret.add(Product(item['id'] ,item['name'], Uri.parse( image['url'] )));
-    }
-    return ret;
-  }
-  catch(e) {
-    return List<Product>.empty();
-  }  
-}
-
 class ProductApi {
+  Future<List<Product>> _fetchProducts({int page =0}) async {
+    try {
+      debugPrint('fetchProducts: page = $page');
+      var url = Uri(
+        scheme: 'https', 
+        host: magentoHost, 
+        path: '/rest/V1/products-render-info', queryParameters: { 
+          'searchCriteria[sortOrders][0][field]': 'entity_id',
+          'searchCriteria[sortOrders][0][direction]':'DESC',
+          'searchCriteria[pageSize]': '20', 
+          'searchCriteria[currentPage]': '$page', 
+          'storeId' :'0', 
+          'currencyCode': 'hkd' 
+        });
+
+      var resp = await http.get(url);
+      var map = jsonDecode( resp.body );
+
+      var ret = <Product>[];
+      for(var item in map['items']) {
+        //print('name: ${item['name']}');
+        var image = item['images'].first;
+        //print(' url: ${image['url']}');
+        ret.add(Product(item['id'] ,item['name'], Uri.parse( image['url'] )));
+      }
+      return ret;
+    }
+    catch(e) {
+      return List<Product>.empty();
+    }  
+  }
+
+
   final StreamController<List<Product>> _streamController = StreamController<List<Product>>();
-  int _currentPage = 1;
   bool _isProcessing = false;
 
-  void start() async {  
+  void fetch(int page) async {  
     if(!_streamController.isClosed && ! _isProcessing) {
       _isProcessing = true;
-      List<Product> products = await fetchProducts( page: _currentPage);
+      List<Product> products = await _fetchProducts( page: page);
       _streamController.add(products);
-      _currentPage++;
       _isProcessing = false;
     }
   }
 
-  void stop() {
+  void dispose() {
     _streamController.close();
-    _currentPage = 1;
     _isProcessing = false;
   }
 
