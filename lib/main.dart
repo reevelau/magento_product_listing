3e55fc5337436212974b05cb514ed13c6b5515f4
Reeve Lau <reevelau@gmail.com>
2023-06-12 15:45:57 +0800

    initial checkin

---
+++ lib/main.dart
@@ -0,0 +1,194 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const String magentoHost = 'www.broadwaylifestyle.com';
+const String magentoLogo = 'https://www.broadwaylifestyle.com/media/logo/websites/1/logo.png';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final ProductApi productApi = ProductApi();
+  final ScrollController _scrollController = ScrollController();
+  final List<Product> _products = [];
+
+  void _onScroll() {
+    if (_scrollController.position.pixels >= _scrollController.position.maxScrollExtent - 1000) {
+      productApi.start();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    productApi.productStream.listen((List<Product> newProducts) {
+      setState(() {
+        _products.addAll(newProducts);
+      });
+    });
+    _scrollController.addListener(_onScroll);
+    productApi.start();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    productApi.stop();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: CustomScrollView(
+          controller: _scrollController,
+          
+          slivers: <Widget>[
+             SliverAppBar(
+              backgroundColor: Colors.white,
+              pinned: true,
+              snap: true,
+              floating: true,
+              expandedHeight: 160,
+              flexibleSpace:  FlexibleSpaceBar(
+                title: Padding(
+                  padding: const EdgeInsets.only(top:10),
+                  child: Image.network(magentoLogo),
+                ),
+                background:   Container(decoration: const BoxDecoration(gradient: LinearGradient(begin: Alignment.centerLeft, end: Alignment.centerRight, colors: [ Colors.white,Colors.white, ]))),
+              ),
+            ),
+            SliverGrid( 
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 0,
+                crossAxisSpacing: 0,
+                childAspectRatio: 0.5,
+              ),
+              delegate: SliverChildBuilderDelegate( (context,index) {
+              debugPrint('SliverChildBuilderDelegate ()');
+              if (index >= _products.length) {
+                return null;
+              }
+              var item = _products[index];
+              return Column(
+                
+                children: [
+                  Image.network(item.imageUrl.toString(), height: 184.0, width: 184.0, fit: BoxFit.fitHeight),
+                  SizedBox(width: 184, child: Text(item.name)),
+                  Expanded(child: Container()),
+                  OutlinedButton.icon(
+                    style: OutlinedButton.styleFrom(
+                      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+                      foregroundColor: Colors.grey,
+                      side: const BorderSide(
+
+                        color: Colors.grey
+                        )  
+                    ),
+                    onPressed: (){}, icon: const Icon(Icons.shopping_cart), label: const Text('加入購物車'),
+                    )
+                ],
+              );
+            }, childCount: _products.length ),
+            
+            )
+          ],
+        ),
+    );
+  }
+}
+
+
+
+// api
+class Product {
+  String name;
+  Uri imageUrl;
+  int id;
+  Product(this.id, this.name, this.imageUrl);
+}
+
+Future<List<Product>> fetchProducts({int page =0}) async {
+  try {
+    debugPrint('fetchProducts: page = $page');
+    var url = Uri(
+      scheme: 'https', 
+      host: magentoHost, 
+      path: '/rest/V1/products-render-info', queryParameters: { 
+        'searchCriteria[sortOrders][0][field]': 'entity_id',
+        'searchCriteria[sortOrders][0][direction]':'DESC',
+        'searchCriteria[pageSize]': '20', 
+        'searchCriteria[currentPage]': '$page', 
+        'storeId' :'0', 
+        'currencyCode': 'hkd' 
+      });
+
+    var resp = await http.get(url);
+    var map = jsonDecode( resp.body );
+
+    var ret = <Product>[];
+    for(var item in map['items']) {
+      //print('name: ${item['name']}');
+      var image = item['images'].first;
+      //print(' url: ${image['url']}');
+      ret.add(Product(item['id'] ,item['name'], Uri.parse( image['url'] )));
+    }
+    return ret;
+  }
+  catch(e) {
+    return List<Product>.empty();
+  }  
+}
+
+class ProductApi {
+  final StreamController<List<Product>> _streamController = StreamController<List<Product>>();
+  int _currentPage = 1;
+  bool _isProcessing = false;
+
+  void start() async {  
+    if(!_streamController.isClosed && ! _isProcessing) {
+      _isProcessing = true;
+      List<Product> products = await fetchProducts( page: _currentPage);
+      _streamController.add(products);
+      _currentPage++;
+      _isProcessing = false;
+    }
+  }
+
+  void stop() {
+    _streamController.close();
+    _currentPage = 1;
+    _isProcessing = false;
+  }
+
+  Stream<List<Product>> get productStream => _streamController.stream;
+}
